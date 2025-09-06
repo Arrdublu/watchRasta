bf95c805b9add2f38b8213e826d253c22825fc9b
Ramone Wynter <hi@arrdublu.us>
2025-09-06 21:19:03 +0000

    remove Public API keys

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBzB3twSVNttGgGQ9yhMQb0HPo5ywMMpI8",
-    authDomain: "watchrasta.firebaseapp.com",
-    projectId: "watchrasta",
-    storageBucket: "watchrasta.firebasestorage.app",
-    messagingSenderId: "839347362900",
-    appId: "1:839347362900:web:ec8b6be6e0b58bae17e1c1",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
