6daa835a1fc878c64a750dfdae6f77db1707d934
Your Name <hi@arrdublu.us>
2025-09-07 17:51:04 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.watchrasta.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   experimental: {
