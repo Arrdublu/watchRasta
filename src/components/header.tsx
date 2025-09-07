d3ca0bbc5aa08a2e2fb56a8bf6ab725e859a8529
Ramone Wynter <hi@arrdublu.us>
2025-09-07 20:37:02 +0000

    remove (_for element span_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -66,8 +66,8 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center">
         <Link href="/" className="mr-6 flex items-center space-x-2">
-          <Image src="https://firebasestorage.googleapis.com/v0/b/watchrasta.firebasestorage.app/o/watchRasta_2024_logo-10.png?alt=media&token=5c16b454-c804-4aa8-8e8f-78115837cc14" alt="watchRasta logo" width={24} height={24} className="h-6 w-6" />
-          <span className="font-bold font-headline text-lg">watchRasta</span>
+          <Image src="https://firebasestorage.googleapis.com/v0/b/watchrasta.appspot.com/o/images%2F3wR-logo.png?alt=media&token=8d272378-752e-40f4-8848-036125b29267" alt="watchRasta logo" width={24} height={24} className="h-6 w-6" />
+          
         </Link>
         <div className="flex flex-1 items-center justify-end">
           <nav className="hidden md:flex md:items-center md:space-x-6 text-sm font-medium">
