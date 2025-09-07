640cde20405d4435823b8ac09b8fd469fab0d911
Ramone Wynter <hi@arrdublu.us>
2025-09-07 21:02:04 +0000

    undo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -37,7 +37,7 @@ export function Footer() {
         <div className="grid grid-cols-2 md:grid-cols-5 gap-8">
           <div className="col-span-2 md:col-span-1 flex items-start flex-col">
             <Link href="/" className="flex items-center space-x-2 mb-4">
-              <Image src="https://firebasestorage.googleapis.com/v0/b/watchrasta.firebasestorage.app/o/watchRasta_2024_logo-10.png?alt=media&token=ff529403-c4ab-4828-81c4-a44f712ca138" alt="watchRasta logo" width={36} height={36} className="h-9 w-9" />
+              <Image src="https://firebasestorage.googleapis.com/v0/b/watchrasta.appspot.com/o/images%2F3wR-logo.png?alt=media&token=8d272378-752e-40f4-8848-036125b29267" alt="watchRasta logo" width={24} height={24} className="h-6 w-6" />
             </Link>
             <p className="text-sm text-muted-foreground">
               © {new Date().getFullYear()} watchRasta. All rights reserved.
