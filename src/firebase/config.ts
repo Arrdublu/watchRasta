d564a19e0fb22feca07358da44683594db5bb99b
Ramone Wynter <hi@arrdublu.us>
2025-12-18 22:14:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5785177965-f69a8",
+  "appId": "1:622424986350:web:58982ba24aa309465d19ff",
+  "apiKey": "AIzaSyDwBa8pEEwVeqLGTvIB9XmAWF8-PrrauGk",
+  "authDomain": "studio-5785177965-f69a8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "622424986350"
+};
