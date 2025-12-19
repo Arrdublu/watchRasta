ec2ce403d0d1ef3067e7cc5f9ff1f1ed302db93e
Ramone Wynter <hi@arrdublu.us>
2025-12-19 07:31:13 +0000

    scale logo evenly (_for element img_)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -39,7 +39,7 @@ export function Footer() {
         <div className="grid grid-cols-2 md:grid-cols-5 gap-8">
           <div className="col-span-2 md:col-span-1 flex items-start flex-col">
             <Link href="/" className="flex items-center space-x-2 mb-4">
-              <Image src="https://firebasestorage.googleapis.com/v0/b/watchrasta.firebasestorage.app/o/watchRasta_2024_logo-10.png?alt=media&token=ff529403-c4ab-4828-81c4-a44f712ca138" alt="watchRasta logo" width={24} height={24} className="h-6 w-6" />
+              <Image src="https://firebasestorage.googleapis.com/v0/b/watchrasta.firebasestorage.app/o/watchRasta_2024_logo-10.png?alt=media&token=ff529403-c4ab-4828-81c4-a44f712ca138" alt="watchRasta logo" width={24} height={24} className="h-6 w-auto" />
             </Link>
           </div>
 
